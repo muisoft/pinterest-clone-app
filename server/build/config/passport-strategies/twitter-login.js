75d4fd4cf9d14eb8c3d707bfdc2a13609a363874
muideen <muisoft@github.com>
2018-07-11 00:16:41 +0100

    buil server

--- server/build/config/passport-strategies/twitter-login.js
+++ server/build/config/passport-strategies/twitter-login.js
@@ -9,9 +9,9 @@ var _passportTwitter = require('passport-twitter');
 var _models = require('../../models');
 
 exports.default = new _passportTwitter.Strategy({
-    consumerKey: 'acPxfhlI6TyhKQaXuO3zP88H6',//process.env.TWITTER_API_KEY,
-    consumerSecret: 'V6MQaCtskzdP926reNzpbDk2drlCSDuCIeDlvM5fl7ukGHUPeA',//process.env.TWITTER_API_SECRET,
-    callbackURL: '/twitter/auth/'//process.env.CALLBACK_URL
+    consumerKey: process.env.TWITTER_API_KEY,
+    consumerSecret: process.env.TWITTER_API_SECRET,
+    callbackURL: process.env.CALLBACK_URL
 }, function (accessToken, refreshToken, profile, done) {
     _models.User.findOneAndUpdate({ twitterId: profile.id }, { twitterId: profile.id, username: profile.username, password: profile.id }, { upsert: 'true' }).exec(function (err, user) {
         if (err) return done(err);
